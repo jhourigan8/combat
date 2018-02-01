7d0ec754a42738735f4e4668bbe6fda5f0563fcc
Jack <jackehourigan@gmail.com>
2018-02-01 22:52:49 +0000

    loop and attack time added# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Changes to be committed:#	modified:   Combat.java#

--- Combat.java
+++ Combat.java
@@ -1,15 +1,30 @@
 class Combat{
 
+boolean playing = true;
+int moveVal = 0;
+
 class Game{
    public Game();
    public void runGame(){
-      Weapon startSword = new Weapon("Rustic Blade", "An old iron sword", 8, 0);
+      Weapon startSword = new Weapon("Rustic Blade", "An old iron sword", 8, 0, 5);
       Fighter player = new Fighter("Dopey", startSword);
+      while(playing == true){
+         if(moveVal >= 0){
+            //player move
+         }else{
+            //opponent move
+         }
+      }
+      
    }
 }
 
 class Fighter{
    String name;
+   int strength;
+   int health;
+   int dexterity;
+   
    Weapon weapon;
    public Fighter(String name, Weapon weapon){
    this.name = name;
@@ -18,14 +33,16 @@ class Fighter{
 }
 
 class Weapon extends Item{
-   public Weapon(String name, String description, int damage, int value){
+   public Weapon(String name, String description, int damage, int value, int useTime){
    this.name = name;
    this.description = description;
    this.damage = damage;
    this.value = value;
+   this.useTime = useTime;
    }
    int damage;
    int value;
+   int useTime;
 }
 
 class Item{
