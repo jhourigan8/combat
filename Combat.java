3c9c8054a2574fe342c2b69ec6b045d14969bc1e
jhourigan8 <35819187+jhourigan8@users.noreply.github.com>
2018-02-05 22:30:22 -0500

    Add files via upload
    added orc, orc weapons, fighter stats

--- Combat.java
+++ Combat.java
@@ -1,14 +1,23 @@
+package combatGame;
+
 class Combat{
 
 boolean playing = true;
 int moveVal = 0;
 
 class Game{
-   public Game();
+	int diff;
+   public Game(int diff) {
+	   this.diff = diff;
+   }
    public void runGame(){
       Weapon startSword = new Weapon("Rustic Blade", "An old iron sword", 8, 0, 5);
-      Fighter player = new Fighter("Dopey", startSword);
+      Fighter player = new Fighter("Dopey", startSword, 3, 30, 2);
+      Weapon orcAxe = new Weapon("Orc Axe", "xx", 12, 5, 10);
+      Fighter orc = new Fighter("Evil Orc", orcAxe, 6, 40, 5);
+      
       while(playing == true){
+    	  System.out.println("a great orc approaches!");
          if(moveVal >= 0){
             //player move
          }else{
@@ -21,14 +30,17 @@ class Game{
 
 class Fighter{
    String name;
-   int strength;
-   int health;
-   int dexterity;
+   int str;
+   int hp;
+   int dex;
    
    Weapon weapon;
-   public Fighter(String name, Weapon weapon){
+   public Fighter(String name, Weapon weapon, int str, int hp, int dex){
    this.name = name;
    this.weapon = weapon;
+   this.str = str;
+   this.hp = hp;
+   this.dex = dex;
    }
 }
 
@@ -54,4 +66,6 @@ class Item{
 public static void main(String args[]){
    Game game = new Game();
    game.runGame();
+}
+
 }
